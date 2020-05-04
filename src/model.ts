1216a9cde943f7f3e43a583b36fe6f7634dfea83
EskalonaD <eskalonad@gmail.com>
2020-05-04 05:06:12 +0300

    initial commit braindead crud boilerplate

---
+++ src/model.ts
@@ -0,0 +1,88 @@
+/**
+ * two level data
+ */
+import fs from 'fs';
+
+export class DataStorageFactory {
+    constructor(name: string, schema, baseData, private type) { // schema IS base data // schema is object for now
+
+        const baseFilePath: string = `${__dirname}/base-data/${name}.json`;
+        this.filePath = `${__dirname}/data/${name}.json`;
+
+        this._data = fs.existsSync(this.filePath)
+            ? JSON.parse(fs.readFileSync(this.filePath, 'utf8'))
+            : fs.existsSync(baseFilePath)
+                ? JSON.parse(fs.readFileSync(baseFilePath, 'utf8'))
+                : null;
+
+        if (!fs.existsSync(this.filePath)) fs.writeFileSync(this.filePath, '{}', 'utf8'); // mb null instead of {}?
+
+
+        if (typeof schema === 'object') this.schemaFields = Object.keys(schema);
+
+    }
+
+    private schemaFields;       //paste on post if none is provided at initialization?/
+
+    protected filePath: string;
+    protected _data: any;
+
+    get data() {
+        const remoteData = JSON.parse(fs.readFileSync(this.filePath, 'utf8'));
+
+        if (this._data !== remoteData) {        // could cause problem if multiple simultenious connections to same file occurs;
+            fs.writeFileSync(this.filePath, JSON.stringify(this._data, null, 2), 'utf8');
+        }
+
+        this._data = remoteData;
+        console.log('data parsed');
+        return this._data;
+    }
+
+    set data(value: any) {
+        fs.writeFileSync(this.filePath, JSON.stringify(this._data, null, 2), 'utf8')
+        this._data = value; // mb will work w\o this line??
+        console.log('data saved')
+    }
+
+    post(data: object) {
+        if(this.schemaFields.every(prop => data.hasOwnProperty(prop))) {
+
+        }
+        else {
+            throw Error;
+        }
+
+    }
+
+    put(){
+
+    }
+
+    get() {
+        return this.data; //check if this is actually get. check CRUD API info. mby get one item?
+    }
+
+    delete() {
+
+        fs.unlinkSync(this.filePath);
+
+    }
+
+    postChild() {
+
+    }
+
+    getChild() {
+
+    }
+
+    putChild() {
+
+    }
+
+    deleteChild() {
+
+    }
+
+}
