9934996c9e0dfd862f646c4dde6e07050c284a56
EskalonaD <eskalonad@gmail.com>
2020-05-10 02:11:02 +0300

    chore: mv paths to configuration file, add some config props

---
+++ src/braindead/configuration.ts
@@ -0,0 +1,8 @@
+export const config = {
+    data_storage_path: `${__dirname}/data`,
+    backup_storage_path: `${__dirname}/backup`,
+    backup: false,
+    auth: false,
+}
+
+// export const testFunc = () => console.log(__dirname)
